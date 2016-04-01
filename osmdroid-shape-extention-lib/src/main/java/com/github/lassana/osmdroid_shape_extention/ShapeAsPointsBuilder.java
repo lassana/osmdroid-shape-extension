39334732f6c9726e4672f26c0b6342e8200f2cd2
Nikolai Doronin <lassana.nd@gmail.com>
2016-04-01 20:13:12 +0300

    Allow setting the step value.

--- osmdroid-shape-extention-lib/src/main/java/com/github/lassana/osmdroid_shape_extention/ShapeAsPointsBuilder.java
+++ osmdroid-shape-extention-lib/src/main/java/com/github/lassana/osmdroid_shape_extention/ShapeAsPointsBuilder.java
@@ -1,8 +1,10 @@
 package com.github.lassana.osmdroid_shape_extention;
 
 import android.support.annotation.NonNull;
+
 import net.sf.geographiclib.Geodesic;
 import net.sf.geographiclib.GeodesicData;
+
 import org.osmdroid.util.GeoPoint;
 
 import java.util.LinkedList;
@@ -18,20 +20,34 @@ public class ShapeAsPointsBuilder {
 
     private final List<GeoPoint> points;
     private final Geodesic wgs84;
+    private int step;
 
     public ShapeAsPointsBuilder() {
         points = new LinkedList<>();
         wgs84 = Geodesic.WGS84;
+        step = 1;
     }
 
-    public ShapeAsPointsBuilder(@NonNull  List<GeoPoint> startList) {
+    public ShapeAsPointsBuilder(@NonNull List<GeoPoint> startList) {
         points = startList;
         wgs84 = Geodesic.WGS84;
+        step = 1;
+    }
+
+    /**
+     * Sets the azimuth step value.
+     */
+    public ShapeAsPointsBuilder withStep(final int step) {
+        if (step <= 0) {
+            throw new IllegalArgumentException("\"step\" value cannot be negative or nil");
+        }
+        this.step = step;
+        return this;
     }
 
     /**
      * Generates points for a great circle (just one next point).
-     * <p>
+     * <p/>
      * Sample geo-data:
      * <pre>
      * NZB172 1   364602.5S 1745003.6E GRC
@@ -44,7 +60,7 @@ public class ShapeAsPointsBuilder {
 
     /**
      * Generates points for a clockwise arc.
-     * <p>
+     * <p/>
      * Sample geo-data:
      * <pre>
      * NZB172 2   365618.3S 1743309.7E CWA 365219.2S 1742923.5E 5 NM
@@ -61,11 +77,11 @@ public class ShapeAsPointsBuilder {
         double ffaz = f.azi1;
         double tfaz = (((ffaz < 0) && (t.azi1 < 0)) || (t.azi1 > 0)) ? t.azi1 : 360 + t.azi1;
 
-        while ((int)ffaz != (int)tfaz) {
+        while (Math.abs((int) ffaz - (int) tfaz) >= step) {
             GeodesicData llb = wgs84.Direct(centerPoint.getLatitude(), centerPoint.getLongitude(), ffaz, f.s12);
             points.add(new GeoPoint(llb.lat2, llb.lon2));
-            ffaz += 1;
-            if (ffaz>360) {
+            ffaz += step;
+            if (ffaz > 360) {
                 ffaz -= 360;
             }
         }
@@ -77,7 +93,7 @@ public class ShapeAsPointsBuilder {
 
     /**
      * Generates points for a counterclockwise arc.
-     * <p>
+     * <p/>
      * Sample geo-data:
      * <pre>
      * NZB172 4   364106.1S 1743732.9E CCA 363954.5S 1743840.7E 1.5 NM
@@ -91,13 +107,13 @@ public class ShapeAsPointsBuilder {
         GeodesicData f = wgs84.Inverse(centerPoint.getLatitude(), centerPoint.getLongitude(), startPoint.getLatitude(), startPoint.getLongitude());
         GeodesicData t = wgs84.Inverse(centerPoint.getLatitude(), centerPoint.getLongitude(), endPoint.getLatitude(), endPoint.getLongitude());
 
-        double ffaz = (f.azi1 < 0) ? 360 + f.azi1: f.azi1;
-        double tfaz = (t.azi1 < 0) ? 360 + t.azi1: t.azi1;
+        double ffaz = (f.azi1 < 0) ? 360 + f.azi1 : f.azi1;
+        double tfaz = (t.azi1 < 0) ? 360 + t.azi1 : t.azi1;
 
-        while (Math.abs((int)ffaz) != Math.abs((int)tfaz)) {
-            GeodesicData llb = wgs84.Direct(centerPoint.getLatitude(), centerPoint.getLongitude(), ffaz,  f.s12);
+        while (Math.abs(Math.abs((int) ffaz) - Math.abs((int) tfaz)) >= step) {
+            GeodesicData llb = wgs84.Direct(centerPoint.getLatitude(), centerPoint.getLongitude(), ffaz, f.s12);
             points.add(new GeoPoint(llb.lat2, llb.lon2));
-            ffaz -= 1;
+            ffaz -= step;
             if (ffaz < 0) {
                 ffaz += 360;
             }
@@ -110,14 +126,14 @@ public class ShapeAsPointsBuilder {
 
     /**
      * Generates points for a circle.
-     * <p>
+     * <p/>
      * Sample geo-data:
      * <pre>
      * NZA347_S 1    CIR 391722.4S 1753345.6E 3 NM
      * </pre>
      */
     public ShapeAsPointsBuilder CIR(@NonNull GeoPoint centerPoint, double dist) {
-        for ( double ffaz = 1; ffaz < 360; ffaz += 1 ){
+        for (double ffaz = 1; ffaz < 360; ffaz += 1) {
             GeodesicData llb = wgs84.Direct(centerPoint.getLatitude(), centerPoint.getLongitude(), ffaz, dist);
             points.add(new GeoPoint(llb.lat2, llb.lon2));
         }
@@ -126,7 +142,7 @@ public class ShapeAsPointsBuilder {
 
     /**
      * Generates points for a rhumbline.
-     * <p>
+     * <p/>
      * Not implemented.
      */
     protected ShapeAsPointsBuilder RHL() {
@@ -135,7 +151,7 @@ public class ShapeAsPointsBuilder {
 
     /**
      * Generates points for a geoborder i.e. a line following the road, etc.
-     * <p>
+     * <p/>
      * Not implemented.
      */
     protected ShapeAsPointsBuilder FNT() {
